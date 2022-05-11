33c42efe6b2db5007c2a60a3cfea62cc692bf7ee
andjipandji99 <andja.anci99@gmail.com>
2022-05-11 14:17:06 +0300

    Dodat restcontroller
    Dodat restcontroller

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\rest\CityRestController;
+use App\Http\Controllers\rest\PersonRestController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResources([
+    '/people'=>PersonRestController::class,
+    '/cities'=>CityRestController::class,
+]);
